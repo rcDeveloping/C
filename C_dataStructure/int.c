3cfc999003f7d8ba48d07aba7d94ff068eb9c34b
rcflorestal <rcflorestal@yahoo.com.br>
2022-03-30 00:10:37 -0300

    int

--- C_dataStructure/int.c
+++ C_dataStructure/int.c
@@ -5,7 +5,6 @@
 
 int main(void) {
 	int i;
-	int n = INT_MAX;
 	
 	//unsigned int
 	unsigned int u = UINT_MAX;
@@ -14,15 +13,13 @@ int main(void) {
 	
 	//short int
 	short int shortInt;
-	short int shortIntMax = SHRT_MAX;
 	
 	//long int on windows is 32 bits and on Linux is 64 bits
 	long int longInt;
-	long int longIntMax = LONG_MAX;
+
 	
 	//long long int
 	long long int longLongInt;
-	long long int longLongIntMax = LLONG_MAX;
 	
 	
 	//size_t from stdlib.h
@@ -44,17 +41,17 @@ int main(void) {
 	to output the value of an int variable, you must use the format 
 	specifier %d or %i for decimal numbers
 	*/
-	printf("Minimum Value of int: %d\n", i);
+	printf("The Minimum Value of unsigned int: %u\n", i);
 	
 	//Maximum value of int
-	printf("Maximum Value of int: %d\n", n);
+	printf("The Maximum Value of signed int: %d\n", INT_MAX);
 	
 	/*
 	Maximum value of unsigned int
 	to output the value of an unsigned int variable, you must use the
 	format specifier %u for decimal numbers
 	*/
-	printf("\nThe size of unsigned int: %zu bytes / %zu bits\n", sizeof u, \
+	//printf("The size of unsigned int: %zu bytes / %zu bits\n", sizeof u, \
 	sizeof u * 8);
 	printf("The Maximum value of unsigned int is: %u\n", u);
 	
@@ -66,14 +63,20 @@ int main(void) {
 	// short int
 	printf("\nThe size of short int is: %zu bytes / %zu bits\n", sizeof \
 	shortInt, sizeof shortInt * 8);
-	printf("The Minimum value of short int is: %u\n", shortInt);
-	printf("The Maximum value of short int is: %u\n", shortIntMax);
+	printf("The Minimum value of short int is: %d\n", SHRT_MIN);
+	printf("The Maximum value of short int is: %d\n", SHRT_MAX);
+	
+	//long int
+	printf("\nThe size of long int is: %zu bytes / %zu bits\n", sizeof\
+	 longInt, sizeof longInt * 8);
+	printf("The minimum value of long int is: %ld\n", LONG_MIN);
+	printf("The maximum value of long max is: %ld\n", LONG_MAX);
 	
 	//long long int
 	printf("\nThe size of long long int is: %zu bytes / %zu bits\n", sizeof\
 	 longLongInt, sizeof longLongInt * 8);
-	printf("The minimum value of long long int is: %lld\n", longLongInt);
-	printf("The maximum value of long long max is: %lld\n", longLongIntMax);
+	printf("The minimum value of long long int is: %lld\n", LLONG_MIN);
+	printf("The maximum value of long long max is: %lld\n", LLONG_MAX);
 	
 	//size_t
 	printf("\nThe size of size_t is: %zu bytes / %zu bits\n", sizeof st, \
